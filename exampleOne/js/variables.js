c7d05ab52075ec2178f72f09b4e1248ef8a13d91
yandrea888 <yandream888@gmail.com>
2020-09-12 08:39:32 -0500

    add exampleOne folder

---
+++ exampleOne/js/variables.js
@@ -0,0 +1,11 @@
+//scope
+let nombre = 'Yuly'
+let edad = 35
+estatura = 1.89
+const fechaEspecial = "28-03-99"
+const estado_casado = true
+
+//'Tu nombre es: ' + nombre + ' y tu edad es: ' + edad
+
+//literar string
+document.write(`Tu nombre es ${nombre} y tu edad es ${edad} años y más`)
